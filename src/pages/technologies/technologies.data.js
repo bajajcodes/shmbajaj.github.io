ef17331864151a04ddfbd4b262fa72e588cd08d5
ShubhamBajaj <shmbajaj32@gmail.com>
2022-06-27 13:44:38 +0530

    feat: all pages added

---
+++ src/pages/technologies/technologies.data.js
@@ -0,0 +1,80 @@
+const technologies = [
+  {
+    id: "html5",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    title: "html5",
+    style: "shadow-orange-500",
+  },
+  {
+    id: "css3",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    title: "css3",
+    style: "shadow-blue-500",
+  },
+  {
+    id: "javascript",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    title: "javascript",
+    style: "shadow-yellow-500",
+  },
+  {
+    id: "bootstrap",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/bootstrap/bootstrap-original.svg",
+    title: "bootstrap",
+    style: "shadow-purple-500",
+  },
+  {
+    id: "react",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    title: "react",
+    style: "shadow-blue-600",
+  },
+  {
+    id: "reactrouter",
+    source: "https://raw.githubusercontent.com/shmbajaj/files-hosted/master/download-removebg-preview.png",
+    title: "reactrouter",
+    style: "shadow-red-500",
+  },
+  {
+    id: "redux",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+    title: "redux",
+    style: "shadow-violet-600",
+  },
+  {
+    id: "firebase",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain.svg",
+    title: "firebase",
+    style: "shadow-yellow-500",
+  },
+  {
+    id: "tailwindcss",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-plain.svg",
+    title: "tailwindcss",
+    style: "shadow-sky-400",
+  },
+  {
+    id: "git",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    title: "git",
+    style: "shadow-orange-500",
+  },
+  {
+    id: "csharp",
+    source:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg",
+    title: "csharp",
+    style: "shadow-purple-500",
+  },
+];
+
+export { technologies };
