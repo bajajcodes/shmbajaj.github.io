cc256ef5869b9393885e47182bfe33badeafcda7
ShubhamBajaj <shmbajaj32@gmail.com>
2022-06-27 13:44:17 +0530

    feat: header and social links component added

---
+++ src/components/header/header.data.js
@@ -0,0 +1,29 @@
+const links = [
+  {
+    id: "home",
+    text: "home",
+    link: "home",
+  },
+  {
+    id: "about",
+    text: "about",
+    link: "about",
+  },
+  {
+    id: "portfolio",
+    text: "portfolio",
+    link: "portfolio",
+  },
+  {
+    id: "technologies",
+    text: "technologies",
+    link: "technologies",
+  },
+  {
+    id: "contact",
+    text: "contact",
+    link: "contact",
+  },
+];
+
+export { links };
---
+++ src/components/socialLinks/icons/index.js
@@ -0,0 +1,4 @@
+export { Github as GithubIcon } from "./Github";
+export { Linkedin as LinkedinIcon } from "./Linkedin";
+export { Persona as PersonaIcon } from "./Persona";
+export { Twitter as TwitterIcon } from "./Twitter";
